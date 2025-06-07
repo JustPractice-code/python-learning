17aca3d7a55e43b12f71defbd3126dcfb6c6b774
JustPractice-code <learn4something@gmail.com>
2025-06-07 19:58:06 +0530

    Day-3 done(concept cleared)

--- day3.py
+++ day3.py
@@ -1,3 +1,11 @@
-num = int(input("Enter any number : "))
-for i in range(1,num):
-    print(i)
+# num = int(input("Enter any number : "))
+# fact = 1
+# for i in range(1,num+1):
+#     fact *= i
+# print(fact)
+
+while True:
+    ans = int(input("Guess one number : "))
+    if ans == 8:
+        break
+    
